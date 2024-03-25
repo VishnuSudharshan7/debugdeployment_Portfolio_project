6f062a88b90f44e1fbd77b3e9947fb2dea32b124
VishnuSudharshan7 <155740228+VishnuSudharshan7@users.noreply.github.com>
2024-03-25 18:23:18 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,17 @@
+    var typed= new Typed(".text",{
+    strings:["Welcome to my  Portfolio Page..."],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+})
+
+
+var typed= new Typed(".txt",{
+    strings:["Lets Work Together!"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+})
+
